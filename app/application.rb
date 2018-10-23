20afb66eefff16b98839fc89596d73f2a6a3728b
Sherry Martin <sm674@cornell.edu>
2018-10-23 14:55:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,29 +10,12 @@ class Application
       resp.write item.price
     else
       resp.write "Item not found"
+      resp.status = 400
+    end
+    else
+      resp.write "Route not found"
       resp.status = 404
     end
-
-    resp.finish
-  end
-end
-
-
-
-class Application
-
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-
-    if req.path.match(/items/)
-
-      item_title = req.path.split("/items/<ITEM NAME>").last #turn /songs/Sorry into Sorry
-      song = @@songs.find{|s| s.title == song_title}
-
-      resp.write song.artist
-    end
-
     resp.finish
   end
 end
