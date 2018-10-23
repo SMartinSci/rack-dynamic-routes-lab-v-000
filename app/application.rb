82a0255a8eadfef99c2f86a2bc4fe43b348c6b0c
Sherry Martin <sm674@cornell.edu>
2018-10-23 14:54:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,17 +1,19 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-    
+      item_name = req.path.split("/items/").last
+    if item = @@items.find{|s| s.name == item_name}
+      
       resp.write "You requested the items"
     else
       resp.write "Item not found"
       resp.status = 404
     end
- 
+
     resp.finish
   end
 end
@@ -19,19 +21,19 @@ end
 
 
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path.match(/items/)
- 
+
       item_title = req.path.split("/items/<ITEM NAME>").last #turn /songs/Sorry into Sorry
       song = @@songs.find{|s| s.title == song_title}
- 
+
       resp.write song.artist
     end
- 
+
     resp.finish
   end
 end
